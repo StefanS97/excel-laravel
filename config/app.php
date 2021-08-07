c77af12742c85476b71f0e1129c6c8eaff109e63
StefanS97 <stefan.stojkov97@gmail.com>
2021-08-07 17:40:43 +0200

    Excel installed

--- config/app.php
+++ config/app.php
@@ -162,6 +162,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        // Excel-Laravel
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -228,6 +231,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Excel-Laravel
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
